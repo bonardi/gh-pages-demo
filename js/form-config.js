30a3a2fa21285a6277c16ed3276dc79fd717a080
Belmont <defontik@gmail.com>
2026-03-10 18:17:45 +0000

    Add configurable MailerLite form injection via separate config file

---
+++ js/form-config.js
@@ -0,0 +1,5 @@
+// configuration file for subscription/signup form
+// set `enabled` to true to show the embed form or false to disable it.
+const signupConfig = {
+    enabled: true // flip to false to hide the form
+};
--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,15 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// subscription form injection logic
+if (typeof signupConfig !== 'undefined' && signupConfig.enabled) {
+    document.addEventListener('DOMContentLoaded', function() {
+        var container = document.getElementById('signup-container');
+        if (container) {
+            container.innerHTML = '\n            <form class="ml-block-form" action="https://assets.mailerlite.com/jsonp/2178591/forms/181578117957027664/subscribe" data-code="" method="post" target="_blank">\n                <div class="row input-group-newsletter">\n                    <div class="col">\n                        <input class="form-control" name="fields[email]" type="email" placeholder="Enter email address..." aria-label="Enter email address..." required />\n                    </div>\n                    <div class="col-auto">\n                        <button class="btn btn-primary" type="submit">Notify Me!</button>\n                    </div>\n                </div>\n            </form>\n            ';
+        }
+    });
+} else {
+    // form is disabled - you can put alternative content or leave empty
+}
